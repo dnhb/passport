0dd29535eb4e3d746efc832c0ed74c3f91b4f70f
Jan Hopman <j.hopman@hypotheekbond.nl>
2021-07-20 16:00:07 +0200

    Add config.

--- src/PassportServiceProvider.php
+++ src/PassportServiceProvider.php
@@ -93,6 +93,7 @@ class PassportServiceProvider extends ServiceProvider
         $this->mergeConfigFrom(__DIR__.'/../config/passport.php', 'passport');
 
         Passport::setClientUuids($this->app->make(Config::class)->get('passport.client_uuids', false));
+        Passport::setRevokeRefreshTokens($this->app->make(Config::class)->get('passport.revoke_refresh_tokens'));
 
         $this->registerAuthorizationServer();
         $this->registerResourceServer();
